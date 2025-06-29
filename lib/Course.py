9242b3fa5a71baad4ebaf738abd7fbae79f064f3
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-30 00:18:33 +0300

    added lab

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,9 @@
 class Course:
-    pass
+    
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        return f"{self.title} | {self.schedule} | {self.description}"
